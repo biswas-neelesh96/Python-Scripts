9f390c8ff4bca562be9669de7495b05fbbbd2d82
Khyati Parashar <58394945+khyati-parashar@users.noreply.github.com>
2020-10-01 00:20:10 +0530

    Basic calculator
    This is a simple addition calculator using Tkinter.

---
+++ calculator.py
@@ -0,0 +1,72 @@
+from tkinter import *
+
+root = Tk()
+root.title("Calculator")
+
+e = Entry(root, width = 35, borderwidth = 5, bg="teal", fg="white")
+e.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
+
+def button_click(num):
+    current= e.get()
+    e.delete(0, END)
+    e.insert(0, str(current)+str(num))
+
+def button_clear():
+    e.delete(0, END)
+
+def button_add():
+    first_num = e.get()
+    global f_num
+    f_num = int(first_num)
+    e.delete(0, END)
+
+
+def button_equal():
+    second_num= e.get()
+    e.delete(0, END)
+    e.insert(0, f_num + int(second_num))
+
+
+# defining buttons
+
+button_1= Button(root, text="1", padx=40, pady=20, bg="teal", fg="white", command=lambda: button_click(1))
+button_2= Button(root, text="2", padx=40, pady=20, bg="teal", fg="white", command=lambda: button_click(2))
+button_3= Button(root, text="3", padx=40, pady=20, bg="teal", fg="white", command=lambda: button_click(3))
+
+button_4= Button(root, text="4", padx=40, pady=20, bg="teal", fg="white", command=lambda: button_click(4))
+button_5= Button(root, text="5", padx=40, pady=20, bg="teal", fg="white", command=lambda: button_click(5))
+button_6= Button(root, text="6", padx=40, pady=20, bg="teal", fg="white", command=lambda: button_click(6))
+
+button_7= Button(root, text="7", padx=40, pady=20, bg="teal", fg="white", command=lambda: button_click(7))
+button_8= Button(root, text="8", padx=40, pady=20, bg="teal", fg="white", command=lambda: button_click(8))
+button_9= Button(root, text="9", padx=40, pady=20, bg="teal", fg="white", command=lambda: button_click(9))
+
+button_0= Button(root, text="0", padx=40, pady=20, bg="teal", fg="white", command=lambda: button_click(0))
+add= Button(root, text="+", padx=39, pady=20, bg="teal", fg="white", command=button_add)
+clear= Button(root, text="Clr", padx=35, pady=20, bg="teal", fg="white", command=button_clear)
+
+equal= Button(root, text="=", padx=143, pady=20, bg="teal", fg="white", command=button_equal)
+
+# button on the screen
+
+button_1.grid(row=3, column=0)
+button_2.grid(row=3, column=1)
+button_3.grid(row=3, column=2)
+
+button_4.grid(row=2, column=0)
+button_5.grid(row=2, column=1)
+button_6.grid(row=2, column=2)
+
+button_7.grid(row=1, column=0)
+button_8.grid(row=1, column=1)
+button_9.grid(row=1, column=2)
+
+button_0.grid(row=4, column=0)
+add.grid(row=4, column=1)
+clear.grid(row=4, column=2)
+
+equal.grid(row=5, column=0, columnspan=3)
+
+
+
+root.mainloop()
