8cbb45175b5910d70c69a4bbbc35cf80c473c299
BRIJESH KUMAR CHOUDHURY <49182106+brijeshchoudhury99@users.noreply.github.com>
2020-10-18 16:36:27 +0530

    swapping.py

---
+++ swapping.py
@@ -0,0 +1,30 @@
+# Python program to demonstrate 
+# swapping of two variables 
+  
+x = 10
+y = 50
+  
+# Swapping of two variables 
+# Using third variable 
+temp = x 
+x = y 
+y = temp 
+  
+print("Value of x:", x) 
+print("Value of y:", y) 
+
+
+_______________OR_________________
+
+
+x=10
+y=20
+print("before swapping:")
+print(x)
+print(y)
+z=x
+x=y
+y=z
+print("after swapping:")
+print(x)
+print(y)
